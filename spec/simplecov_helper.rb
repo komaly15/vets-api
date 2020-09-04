c65463ad0ee0b3a14b31b9cf43a6b1a7c0abf801
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2020-09-04 10:54:50 -0400

    put code coverage reporter back

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -8,6 +8,47 @@ require 'simplecov'
 
 class SimpleCovHelper
   def self.report_coverage(base_dir: './coverage_results')
+    SimpleCov.start 'rails' do
+      skip_check_coverage = ENV.fetch('SKIP_COVERAGE_CHECK', 'false')
+
+      track_files '**/{app,lib}/**/*.rb'
+
+      add_filter 'app/controllers/concerns/accountable.rb'
+      add_filter 'config/initializers/clamscan.rb'
+      add_filter 'lib/config_helper.rb'
+      add_filter 'lib/feature_flipper.rb'
+      add_filter 'lib/gibft/configuration.rb'
+      add_filter 'lib/ihub/appointments/response.rb'
+      add_filter 'lib/salesforce/configuration.rb'
+      add_filter 'lib/vet360/address_validation/configuration.rb'
+      add_filter 'lib/search/response.rb'
+      add_filter 'lib/vet360/exceptions/builder.rb'
+      add_filter 'lib/vet360/response.rb'
+      add_filter 'modules/claims_api/app/controllers/claims_api/v0/forms/disability_compensation_controller.rb'
+      add_filter 'modules/claims_api/app/controllers/claims_api/v1/forms/disability_compensation_controller.rb'
+      add_filter 'modules/claims_api/app/swagger/*'
+      add_filter 'modules/claims_api/lib/claims_api/health_checker.rb'
+      add_filter 'lib/bip_claims/configuration.rb'
+      add_filter 'version.rb'
+
+      add_group 'Policies', 'app/policies'
+      add_group 'Serializers', 'app/serializers'
+      add_group 'Services', 'app/services'
+      add_group 'Swagger', 'app/swagger'
+      add_group 'Uploaders', 'app/uploaders'
+      add_group 'AppealsApi', 'modules/appeals_api/'
+      add_group 'ClaimsApi', 'modules/claims_api/'
+      add_group 'OpenidAuth', 'modules/openid_auth/'
+      add_group 'VBADocuments', 'modules/vba_documents/'
+      add_group 'Veteran', 'modules/veteran/'
+      add_group 'VeteranVerification', 'modules/veteran_verification/'
+      add_group 'OpenidAuth', 'modules/openid_auth/'
+      add_group 'VAOS', 'modules/vaos/'
+
+      minimum_coverage(90) unless skip_check_coverage
+      refuse_coverage_drop unless skip_check_coverage
+      merge_timeout(3600)
+    end
     new(base_dir: base_dir).merge_results
   end
 
